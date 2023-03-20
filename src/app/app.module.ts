9d81f545cde66a3622738d50f386c13fc314a2a3
ngngocTuan07 <ngngoctuan27@gmail.com>
2023-03-20 21:54:11 +0700

    push 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser'
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiceComponent } from './content/service/service.component';
+import { AboutComponent } from './content/about/about.component';
+import { ContactComponent } from './content/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ServiceComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'about', component: AboutComponent },
+      {path: 'content', component: ContentComponent},
+      {path: 'service', component: ServiceComponent},
+      {path: 'contact', component: ContactComponent},
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
